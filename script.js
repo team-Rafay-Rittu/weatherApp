d1be4a4dfbbac4384c091091844fa29408017421
rberman6 <rittuberman@gmail.com>
2023-04-02 16:44:50 -0600

    h1 size adjusted and removed the word please

--- script.js
+++ script.js
@@ -2,8 +2,8 @@
 const weatherApp = {};
 
 // store the api Key
-// weatherApp.apiKey = "DwK5l1uPAjh4A3DfJSFThmsSvZD1jQKy" Rittu's back up key
-weatherApp.apiKey = "iKLTvoFgSq9sc5BKM37ihFtikGNp351H"
+weatherApp.apiKey = "DwK5l1uPAjh4A3DfJSFThmsSvZD1jQKy" 
+// weatherApp.apiKey = "iKLTvoFgSq9sc5BKM37ihFtikGNp351H" Rafay's back up key
 
 
 // ** ----------FUNCTION TO GET WEATHER DATA FOR TOP 50 CITIES----------** //
@@ -226,6 +226,7 @@ weatherApp.getCityWeather = (cityData) => {
             }
         })
         .then((cityWeather) => {
+            console.log(cityWeather);
             // the API result contains weatherData, we have to attach city name and country information to the weatherData
             cityWeather[0].Country = cityData.Country;
             cityWeather[0].EnglishName = cityData.EnglishName;
